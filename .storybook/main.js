eae9111814b3b92f7db0a97dd4e1773c6df86aa6
Alexandr Siryj <theobroma222@gmail.com>
2022-09-11 02:47:26 +0300

    Build a simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,19 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
-  features: {
-    interactionsDebugger: true,
-  },
+    // stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    stories: ["../src/components/**/*.stories.js"],
+    staticDirs: ["../public"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-a11y",
+    ],
+    framework: "@storybook/react",
+    core: {
+        builder: "@storybook/builder-webpack5",
+    },
+    features: {
+        interactionsDebugger: true,
+    },
 };
