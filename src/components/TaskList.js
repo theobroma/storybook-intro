fa7be1d076a1533c3046af2820974c47acb59e60
Alexandr Siryj <theobroma222@gmail.com>
2022-09-11 06:16:57 +0300

    Assemble a composite component step2

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Task from "./Task";
 
@@ -7,20 +8,65 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         onPinTask,
         onArchiveTask,
     };
-
+    // skeleton
+    const LoadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox" />
+            <span className="glow-text">
+                <span>Loading</span> <span>cool</span> <span>state</span>
+            </span>
+        </div>
+    );
     if (loading) {
-        return <div className="list-items">loading</div>;
+        return (
+            <div className="list-items" data-testid="loading" key={"loading"}>
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+                {LoadingRow}
+            </div>
+        );
+    }
+    // no results
+    if (tasks.length === 0) {
+        return (
+            <div className="list-items" key={"empty"} data-testid="empty">
+                <div className="wrapper-message">
+                    <span className="icon-check" />
+                    <p className="title-message">You have no tasks</p>
+                    <p className="subtitle-message">Sit back and relax</p>
+                </div>
+            </div>
+        );
     }
 
-    if (tasks.length === 0) {
-        return <div className="list-items">empty</div>;
-    }
+    // sort order
+    const tasksInOrder = [
+        ...tasks.filter((t) => t.state === "TASK_PINNED"),
+        ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+    ];
 
     return (
         <div className="list-items">
-            {tasks.map((task) => (
+            {tasksInOrder.map((task) => (
                 <Task key={task.id} task={task} {...events} />
             ))}
         </div>
     );
 }
+
+TaskList.propTypes = {
+    /** Checks if it's in loading state */
+    loading: PropTypes.bool,
+    /** The list of tasks */
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+    /** Event to change the task to pinned */
+    onPinTask: PropTypes.func,
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+    loading: false,
+};
