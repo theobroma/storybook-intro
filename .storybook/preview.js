2b11e8e00d88f7aa0be3cea2a1307aff12283da7
Alexandr Siryj <theobroma222@gmail.com>
2022-09-12 05:54:34 +0300

    MSW

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 import "../src/index.css";
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
